bd5c83f1a009d4952b95f1264c3647f05d2d482f
MosesMnz <39951731+MosesMNZ@users.noreply.github.com>
2026-02-15 22:07:06 -0700

    Add MarkLegalMoves method with switch statement to BoardLogic class

--- ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
+++ ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
@@ -12,50 +12,53 @@ namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
 {
     public class BoardLogic
     {
-        /// <summary>
-        /// Reset the board by setting the
-        /// cell properties back to default.
-        /// Encapsulate this method so it can only be
-        /// called from this class.
-        /// </summary>
-        /// <param name="board"></param>
-        /// <returns></returns>
         private BoardModel ResetBoard(BoardModel board)
         {
-            // Use a foreach loop to iterate over every cell
             foreach (CellModel cell in board.Grid)
             {
-                // Set the properties to their defaults
                 cell.IsLegalNextMove = false;
                 cell.PieceOccupyingCell = "";
             }
 
-            // Return the board back to the presentation layer
             return board;
         }
 
-        /// <summary>
-        /// Check if the row/column location is on the board
-        /// Encapsulate this method so it can only be
-        /// called from this class.
-        /// </summary>
-        /// <param name="board"></param>
-        /// <param name="row"></param>
-        /// <param name="col"></param>
-        /// <returns></returns>
         private bool IsOnBoard(BoardModel board, int row, int col)
         {
-            // Get the size of the board
             int size = board.Size;
 
-            // Check if the row is on the board
             bool IsRowSafe = row >= 0 && row < size;
-
-            // Check if the column is on the board
             bool IsColumnSafe = col >= 0 && col < size;
 
-            // Return true if both row and column are safe
             return IsRowSafe && IsColumnSafe;
+        }
+
+        public BoardModel MarkLegalMoves(BoardModel board, CellModel currentCell, string chessPiece)
+        {
+            board = ResetBoard(board);
+
+            switch (chessPiece.ToLower())
+            {
+                case "knight":
+                    break;
+
+                case "rook":
+                    break;
+
+                case "bishop":
+                    break;
+
+                case "queen":
+                    break;
+
+                case "king":
+                    break;
+
+                default:
+                    return board;
+            }
+
+            return board;
         }
     }
 }
