ccd9ef829eddbb33d35817acab2148fdb45143e8
MosesMnz <39951731+MosesMNZ@users.noreply.github.com>
2026-02-15 22:09:16 -0700

    Add knight movement logic to MarkLegalMoves method

--- ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
+++ ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
@@ -12,6 +12,10 @@ namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
 {
     public class BoardLogic
     {
+        /// <summary>
+        /// Reset the board by setting the
+        /// cell properties back to default.
+        /// </summary>
         private BoardModel ResetBoard(BoardModel board)
         {
             foreach (CellModel cell in board.Grid)
@@ -23,6 +27,9 @@ namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
             return board;
         }
 
+        /// <summary>
+        /// Check if the row/column location is on the board
+        /// </summary>
         private bool IsOnBoard(BoardModel board, int row, int col)
         {
             int size = board.Size;
@@ -33,13 +40,39 @@ namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
             return IsRowSafe && IsColumnSafe;
         }
 
+        /// <summary>
+        /// Mark the legal moves for the given piece and location
+        /// </summary>
         public BoardModel MarkLegalMoves(BoardModel board, CellModel currentCell, string chessPiece)
         {
+            // Reset the board
             board = ResetBoard(board);
 
+            // Use a switch statement to determine the behavior of the piece
             switch (chessPiece.ToLower())
             {
                 case "knight":
+
+                    // Set the occupying property for the current cell
+                    board.Grid[currentCell.Row, currentCell.Column].PieceOccupyingCell = "N";
+
+                    // Set possible moves for knight
+                    int[] knightRowMoves = { 2, 2, 1, 1, -1, -1, -2, -2 };
+                    int[] knightColMoves = { 1, -1, 2, -2, 2, -2, 1, -1 };
+
+                    // Loop through the knight moves
+                    for (int i = 0; i < knightRowMoves.Length; i++)
+                    {
+                        int newRow = currentCell.Row + knightRowMoves[i];
+                        int newCol = currentCell.Column + knightColMoves[i];
+
+                        // Check if move is on the board
+                        if (IsOnBoard(board, newRow, newCol))
+                        {
+                            board.Grid[newRow, newCol].IsLegalNextMove = true;
+                        }
+                    }
+
                     break;
 
                 case "rook":
